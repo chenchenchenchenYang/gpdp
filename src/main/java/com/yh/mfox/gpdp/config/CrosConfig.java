c7b6a51348561e363a7bea62919ae1c549e70f8a
chenyangw <wcy19940321@gmail.com>
2021-04-14 16:01:35 +0800

    解决跨域问题

---
+++ src/main/java/com/yh/mfox/gpdp/config/CrosConfig.java
@@ -0,0 +1,38 @@
+package com.yh.mfox.gpdp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+
+/**
+ * @date: 2021年04月14日 15:39
+ * @Description: 跨域
+ */
+@Configuration
+public class CrosConfig {
+    /**
+     * 解决跨域问题
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //1,允许任何来源
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        //2,允许任何请求头
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+        //3,允许任何方法
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        //4,允许凭证
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
