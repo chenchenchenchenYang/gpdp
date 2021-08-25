be55bd51b099e29c6844dddc0a29c6a18d47b15a
chenchenchenchenyang <wcy0321>
2021-08-25 10:30:20 +0800

    netty

---
+++ src/main/java/com/yh/mfox/gpdp/config/NettyServerHandler.java
@@ -0,0 +1,41 @@
+package com.yh.mfox.gpdp.config;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author: Wangcheny
+ * @date: 2021年08月25日 10:11
+ * @Description:
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    private static final Logger log = LoggerFactory.getLogger(NettyServerHandler.class);
+    /**
+     * 客户端连接会触发
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.info("Channel active......");
+    }
+
+    /**
+     * 客户端发消息会触发
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        log.info("服务器收到消息: {}", msg.toString());
+        ctx.write("我是服务端，我收到你的消息了！");
+        ctx.flush();
+    }
+
+    /**
+     * 发生异常触发
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
